6af7c93a697db6cd7ff64e9d41f93fe751a3e45e
Karan682 <mr.karangolwala682002@gmail.com>
2024-12-30 14:56:37 +0000

    url changed

--- greenfuture/src/services/api.js
+++ greenfuture/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://greenfuture-3.onrender.com/api';
 
 
 export const fetchIdeas = () => axios.get(`${API_BASE_URL}/ideas`);
