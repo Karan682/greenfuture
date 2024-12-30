5eac92213d86a517326e41ad6eac8acbc0f57b92
Karan682 <mr.karangolwala682002@gmail.com>
2024-12-30 13:23:10 +0000

    first commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import authenticateToken from './middleware.js';
 import collaborationRoutes from './routes/collaborationRoutes.js';
 import feedbackRoutes from './routes/feedbackRoutes.js';
@@ -14,6 +15,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const _dirname = path.resolve();
 
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -26,6 +28,10 @@ app.use('/api/feedback', feedbackRoutes);
 app.use('/api/collaborations', collaborationRoutes);
 app.use('/api/rewards', rewardRoutes);
 app.use('/api/protected', authenticateToken, protectedRoutes);
+app.use(express.static(path.join(_dirname, "/greenfuture/build")));
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(_dirname,"greenfuture", "build", "index.html"));
+})
 
 const PORT_URL = process.env.PORT || 5000;
 
